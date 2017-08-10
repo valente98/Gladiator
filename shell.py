c62c088c086614d8fb6822b7a07997be22209b39
valente alvarez <valvarez@basecampcodingacademy.org>
2017-08-10 16:05:31 -0500

    created the main function, imported the core into the shell, and created a function for the first gladiator to ask what would he like to do.

---
+++ shell.py
@@ -0,0 +1,13 @@
+import core
+
+def gladiator_1():
+    return ''' Gladiator 1... What would you like to do?\n
+    \t- attack\n
+    \t- pass\n
+    \t- quit\n
+    \t- heal\n'''
+def main():
+    
+
+if __name__ == '__main__':
+    main()
