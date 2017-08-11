b2c92a18dd99113410a471b23906541065635c4a
valente alvarez <valvarez@basecampcodingacademy.org>
2017-08-11 15:25:58 -0500

    Created a functions that will help the program to run

---
+++ core.py
@@ -0,0 +1,17 @@
+def new_gladiator(health, rage, damage_low, damage_high):
+    gladiator = {
+        'health': health,
+        'rage': rage,
+        'damage_low': damage_low,
+        'damage_high': damage_high
+    }
+    return gladiator
+
+
+def attack(attacker, defender):
+    
+
+def heal(gladiator):
+
+
+def is_dead(gladiator):`
