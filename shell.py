282d647bedfb1b1fedcc8df824871c6bbd079844
valente alvarez <valvarez@basecampcodingacademy.org>
2017-08-11 09:46:33 -0500

    deleted a function because the program did not need it

--- shell.py
+++ shell.py
@@ -1,11 +1,4 @@
 import core
-
-def gladiator_1():
-    return ''' Gladiator 1... What would you like to do?\n
-    \t- attack\n
-    \t- pass\n
-    \t- quit\n
-    \t- heal\n'''
 def main():
     
 
