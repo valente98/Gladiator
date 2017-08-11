4c55573966bd91187c046ce21cb946b0f44b8861
valente alvarez <valvarez@basecampcodingacademy.org>
2017-08-11 15:27:26 -0500

    created functions that will ask the user what they want to do with their gladiator.

--- shell.py
+++ shell.py
@@ -1,6 +1,28 @@
 import core
+
+
+def gladiator_1():
+    return input('''Gladiator 1... What would you like to do?\n
+    \t- attack\n
+    \t- pass\n
+    \t- quit\n
+    \t- heal\n
+    ''')
+
+
+def gladiator_2():
+    return input('''Gladiator 2... What would you like to do?\n
+    \t- attack\n
+    \t- pass\n
+    \t- quit\n
+    \t- heal\n
+    ''')
+
+
 def main():
-    
+    while True:
+        
+
 
 if __name__ == '__main__':
     main()
