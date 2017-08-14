ad05e390df4b1efac2a3f377965bad2fe5a964dc
valente alvarez <valvarez@basecampcodingacademy.org>
2017-08-14 11:19:10 -0500

    completed the functions that were created to help with the program

--- core.py
+++ core.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 def new_gladiator(health, rage, damage_low, damage_high):
     gladiator = {
         'health': health,
@@ -9,9 +12,25 @@ def new_gladiator(health, rage, damage_low, damage_high):
 
 
 def attack(attacker, defender):
-    
+    damage_dealt = randint(attacker['damage_low'], attacker['damage_high'])
+    if randint(1, 100) <= attacker['rage']:
+        defender['health'] = (2 * damage_dealt)
+        attacker['rage'] = 0
+    else:
+        defender['health'] -= damage_dealt
+        attacker['rage'] += 15
+    return attacker, defender
+
 
 def heal(gladiator):
+    if gladiator['rage'] >= 10:
+        gladiator['rage'] = max(gladiator['rage'] - 10, 0)
+        gladiator['health'] = min(gladiator['health'] + 5, 100)
+        return gladiator
 
 
-def is_dead(gladiator):`
+def is_dead(gladiator):
+    if gladiator['health'] == 0:
+        return True
+    else:
+        return False
