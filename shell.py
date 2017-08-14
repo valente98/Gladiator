ab40c1eb23eb50a44aef19ad75da1048fa390af5
valente alvarez <valvarez@basecampcodingacademy.org>
2017-08-14 12:48:21 -0500

    created a function that tells the user the details of each gladiator. created two seperate functions that tells the user the detail of each gladiator after the user completes doing what they want their gladiator to do.
    started worink on the main function but its not completed yet.

--- shell.py
+++ shell.py
@@ -1,6 +1,18 @@
 import core
 
 
+def Gladiator_start():
+    print('Gladiator 1: 100 HP ||| 0 rage.. Low damage: 10, High damage: 17')
+    print('gladiator 2: 100 HP ||| 0 rage.. Low damage: 5, High damage: 30')
+
+
+def Gladiators1_details(gladiator):
+    print('Gladiator 1:',gladiator['health'],'HP |||',gladiator['rage'],'Rage')
+
+def Gladiators2_details(gladiator):
+    print('Gladiator 2:',gladiator['health'],'HP |||',gladiator['rage'],'Rage')
+
+
 def gladiator_1():
     return input('''Gladiator 1... What would you like to do?\n
     \t- attack\n
@@ -20,9 +32,37 @@ def gladiator_2():
 
 
 def main():
+    Gladiator_start()
     while True:
-        
+        gladiator = core.new_gladiator(health, rage, damage_low, damage_high)
+        choice = gladiator_1()
+        if choice.lower == 'attack':
 
+        elif choice.lower == 'pass':
+
+        elif choice.lower == 'quit':
+            print('Gladiator 1 looses')
+            print('Gladiator 2 wins')
+            exit()
+        elif choice.lower == 'heal':
+            core.heal(gladiator)
+        else:
+            print('Invalid choice.')
+        Gladiators1_details(gladiator)
+        Gladiators2_details(gladiator)
+        choice = gladiator_2
+        if choice.lower == 'attack':
+
+        elif choice.lower() == 'pass':
+        
+        elif choice.lower() == 'quit':
+            print('Gladiator 2 looses')
+            print('Gladiator 1 wins')
+            exit()
+        elif choice.lower() == 'heal':
+            core.heal(gladiator)
+        else: 
+            print('Invalid choice.')
 
 if __name__ == '__main__':
     main()
